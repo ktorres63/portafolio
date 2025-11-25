84cb01fb95edf046fe74b5e606efeededf893490
ktorres63 <ktorresaro@unsa.edu.pe>
2025-11-24 22:55:12 -0500

    Implement internationalization support and enhance Navbar with language selector

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,9 +6,13 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   i18n: {
-    defaultLocale: 'es',
     locales: ['es', 'en'],
+    defaultLocale: 'es',
+    routing: {
+      prefixDefaultLocale: false,
+    },
   },
+
   vite: {
     plugins: [tailwindcss()]
   }
